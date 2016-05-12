f38103496d1d0493128c86e6714bb71dc61cf504
nirolivier <nirolivier@gmail.com>
2016-05-13 01:22:19 +0400

    Create start, stop script and move the common command into core.py

---
+++ start.py
@@ -0,0 +1,32 @@
+#	======================================================
+#	This script starts a server.
+#	
+#	Author: 	Razafindrabekoto Nirina Olivier
+#	Date:		08/05/2016
+#	Version:	1.0
+#	========================================================
+
+import string
+import const
+
+execfile('./core.py')
+
+#	Program entry point.
+if __name__ == "main":
+	print ''
+	print '======================================================================'
+	print 'Starting the following server'
+	print 'Server name :' + const.APP_SERVER_NAME
+	print '======================================================================'
+	print ''
+	
+	prepareConnection()
+	stopMgServer(const.APP_SERVER_NAME,const.TYPE_SERVER)
+	startMgServer(const.APP_SERVER_NAME,const.TYPE_SERVER,const.WLS_ADMIN_T3_URL)
+
+	print ''
+	print '======================================================================'
+	print 'End of server start process.'
+	print '======================================================================'
+	print ''
+
---
+++ stop.py
@@ -0,0 +1,31 @@
+#	======================================================
+#	This script stops a managed server.
+#	
+#	Author: 	Razafindrabekoto Nirina Olivier
+#	Date:		08/05/2016
+#	Version:	1.0
+#	========================================================
+
+import string
+import const
+
+execfile('./core.py')
+
+#	Program entry point.
+if __name__ == "main":
+	print ''
+	print '======================================================================'
+	print 'Stopping the following server'
+	print 'Server name :' + const.APP_SERVER_NAME
+	print '======================================================================'
+	print ''
+	
+	prepareConnection()
+	stopMgServer(const.APP_SERVER_NAME,const.TYPE_SERVER)
+	
+	print ''
+	print '======================================================================'
+	print 'End of server stop process.'
+	print '======================================================================'
+	print ''
+
